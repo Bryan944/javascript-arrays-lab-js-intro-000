2313eaf49bd02095f25fd8a6f3780240f8f7e8e7
cheerful-response-5821 <bryan.nguyen2494@gmail.com>
2020-04-11 04:54:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,58 @@
-var kittens = [] //define your array here
+var kittens = ["Milo", "Otis", "Garfield"]; //define your array here
 
 // Add your functions and code here
+function cats(){
+  kittens;
+  return kittens;
+}
+cats();
+
+function destructivelyAppendKitten() {
+kittens.push('Ralph')
+  return kittens
+}
+
+function destructivelyPrependKitten(){
+kittens.unshift('Bob');
+  return kittens
+}
+
+
+function destructivelyRemoveLastKitten(){
+kittens.pop();
+  return kittens;
+}
+
+
+function destructivelyRemoveFirstKitten(){
+    kittens.shift();
+  return kittens;
+}
+
+  //var name = 'Broom';
+function appendKitten(name){
+kittens.push(name);
+  return kittens;
+}
+//appendKitten("Broom");
+
+
+//var kittens = ["Milo", "Otis", "Garfield"];
+function prependKitten(name){
+kittens.unshift(name);
+  return kittens;
+}
+
+function removeLastKitten(){
+
+
+}
+
+
+
+function removeFirstKitten(){
+  var kittens = ["Milo", "Otis", "Garfield"];
+var cat;
+cat =  kittens.shift();
+  return kittens;
+}
